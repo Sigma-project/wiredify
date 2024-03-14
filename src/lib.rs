dec83e3870880f02c8ae9480cd4973997e509751
Osumi Akari <kig.osumiakari@gmail.com>
2024-03-14 19:01:03 +0900

    機能をlib.rsに分割

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub fn wiredify (s: String) -> String {
+    let result1: String = s.replace("バ", "ヴァ");
+    let result2: String = result1.replace("ビ", "ヴィ");
+    let result3: String = result2.replace("ブ", "ヴ");
+    let result4: String = result3.replace("ベ", "ヴェ");
+    let result: String = result4.replace("ボ", "ヴォ");
+    return result;
+}
