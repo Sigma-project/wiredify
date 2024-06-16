37884c237e773a7585e105e70d24b8990520d61e
Osumi Akari <kig.osumiakari@gmail.com>
2024-06-16 19:51:56 +0900

    Update to 0.2.1. Thanks #9

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //!
 //! 標準入力で受け取った文字列を「wiredify」し、標準出力へ出力します。
 
-pub fn wiredify(s: String) -> String {
+pub fn wiredify (s: String) -> String {
     let result1: String = s.replace("バ", "ヴァ");
     let result2: String = result1.replace("ビ", "ヴィ");
     let result3: String = result2.replace("ブ", "ヴ");
@@ -14,7 +14,7 @@ pub fn wiredify(s: String) -> String {
 
 /// `dewiredify`も実装しようとしています。
 
-pub fn dewiredify(s: String) -> String {
+pub fn dewiredify (s: String) -> String {
     let result1: String = s.replace("ヴァ", "バ");
     let result2: String = result1.replace("ヴィ", "ビ");
     let result3: String = result2.replace("ヴェ", "ベ");
